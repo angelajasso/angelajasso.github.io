c6bc620e830b4b3eaf3ef36b40b8a53ac1881064
angelajasso <angelajassoma@gmail.com>
2025-09-10 20:50:27 -0600

    adding mode dark

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+const btn = document.getElementById("toggle-theme");
+
+btn.addEventListener("click", () => {
+  console.log("click!"); // 👈 debería aparecer siempre
+  document.body.classList.toggle("dark");
+
+  // Cambia el ícono 🌙/☀️
+  if (document.body.classList.contains("dark")) {
+    btn.textContent = "☀️";
+  } else {
+    btn.textContent = "🌙";
+  }
+});
+
 function toggleMenu() {
   document.getElementById("menu").classList.toggle("show");
-}
-
-function toggleDark() {
-  document.body.classList.toggle("dark");
 }
